a114ab15c7aac1f0d9790f46d715ab9d926ab100
manufer24 <manuelferg13@gmail.com>
2022-11-11 22:32:51 -0400

    links update

--- js/change_language.js
+++ js/change_language.js
@@ -2,9 +2,11 @@ export default function language() {
   const $flagsElements = document.getElementById("flags");
 
   const changeLanguage = async (language) => {
-    if (language === "es") location.href = "/es/index.html";
-    else if (language === "pt") location.href = "/pt/index.html";
-    else location.href = "/index.html";
+    if (language === "es")
+      location.href = "https://manufer24.github.io/portfolio-cv/es/index.html";
+    else if (language === "pt")
+      location.href = "https://manufer24.github.io/portfolio-cv/pt/index.html";
+    else location.href = "https://manufer24.github.io/portfolio-cv/index.html";
   };
 
   $flagsElements.addEventListener("click", (e) =>
