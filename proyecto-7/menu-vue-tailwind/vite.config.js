65a46ee23229fef19c606aba44fa08a893615329
manufer24 <manuelferg13@gmail.com>
2022-11-12 21:02:03 -0400

    links update

--- proyecto-7/menu-vue-tailwind/vite.config.js
+++ proyecto-7/menu-vue-tailwind/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/menu-vue-tailwind",
+  base: "https://manufer24.github.io/portfolio-cv/proyecto-7/menu-vue-tailwind",
   plugins: [vue()],
   resolve: {
     alias: {
