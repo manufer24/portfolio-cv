5fd08745fff1e1ad1864e4326065e93b258fb207
manufer24 <manuelferg13@gmail.com>
2023-06-13 20:03:04 -0400

    animation added

--- js/pre_loader.js
+++ js/pre_loader.js
@@ -2,4 +2,4 @@ export default function preLoader() {
   const $loader = document.querySelector(".pre-loader-container");
   $loader.style.opacity = "0";
   $loader.style.visibility = "hidden";
-}
+} 
