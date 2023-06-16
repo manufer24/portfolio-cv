be09270a48779ba39526cee25aab75e2459c18ee
manufer24 <manuelferg13@gmail.com>
2023-06-15 21:19:13 -0400

    animation added

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,5 +1,7 @@
 export default function scrollReveal() {
-  const reveals = document.querySelectorAll(".reveal");
+  const reveals = document.querySelectorAll(".reveal"),
+    cardTitle = document.querySelectorAll(".card-title"),
+    pShow = document.querySelectorAll(".p-show");
 
   for (let i = 0; i < reveals.length; i++) {
     const windowHeight = window.innerHeight,
@@ -7,5 +9,21 @@ export default function scrollReveal() {
 
     if (revealTop < windowHeight) reveals[i].classList.add("contentReveal");
     else reveals[i].classList.remove("contentReveal");
+  }
+
+  for (let i = 0; i < cardTitle.length; i++) {
+    const windowHeight = window.innerHeight,
+      revealCardTop = cardTitle[i].getBoundingClientRect().top;
+
+    if (revealCardTop < windowHeight) cardTitle[i].classList.add("zoom-out-fade-in");
+    else cardTitle[i].classList.remove("zoom-out-fade-in");
+  }
+
+  for (let i = 0; i < pShow.length; i++) {
+    const windowHeight = window.innerHeight,
+      pRevealTop = pShow[i].getBoundingClientRect().top;
+
+    if (pRevealTop < windowHeight) pShow[i].classList.add("slide-up-fade-in");
+    else pShow[i].classList.remove("slide-up-fade-in");
   }
 }
